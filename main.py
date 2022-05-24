d8801d793af7c4fbbcf2cf8cd470a123cfb8bc03
wioliveira <94582565+wioliveira@users.noreply.github.com>
2022-05-24 13:27:38 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ from kivy.lang import Builder
 from telas import *
 from botoes import *
 import requests
-from bannervenda import BannerVenda
 import os
+import certifi
+from bannervenda import BannerVenda
 from functools import partial
 from myfirebase import MyFirebase
 from bannervendedor import BannerVendedor
 from datetime import date
+
+os.environ["SSL_CERT_FILE"] = certifi.where()
 
 GUI = Builder.load_file("main.kv")
 class MainApp(App):
